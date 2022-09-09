7a01852e5d21f39cc6fc5079d30f83051be05643
SimonEliassonSE <102283683+SimonEliassonSE@users.noreply.github.com>
2022-09-09 15:48:07 +0200

    No more duplicates of same language on person
    code added to prevent duplication of language on person.

--- LexiconMVC/LexiconMVC/Controllers/LanguageController.cs
+++ LexiconMVC/LexiconMVC/Controllers/LanguageController.cs
@@ -3,6 +3,9 @@ using LexiconMVC.Models;
 using LexiconMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace LexiconMVC.Controllers
 {
@@ -53,8 +56,27 @@ namespace LexiconMVC.Controllers
         [HttpPost]   
         public IActionResult AddLanguageToPerson(int personid, int languageid)
         {
+
+            List<Person> people = _context.People.Include(x => x.LanguagesList).ToList();
+
             var person = _context.People.FirstOrDefault(x => x.Id == personid);
             var language = _context.Languages.FirstOrDefault(x => x.Id == languageid);
+
+
+
+            foreach (var aPerson in people)
+            {
+                if (aPerson.Id == personid)
+                {
+                    foreach (var aLanguage in aPerson.LanguagesList)
+                    {
+                        if (aLanguage.Id == languageid)
+                        {
+                            return RedirectToAction("LanguageIndex");
+                        }
+                    }
+                }
+            }
 
             if (ModelState.IsValid)
             {
